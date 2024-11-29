64a44a351ce4fb3a36c9474e89f57ee1b9937b16
LukeHoese <157862417+LukeHoese@users.noreply.github.com>
2024-11-28 18:13:24 -0700

    Update code.js

--- code.js
+++ code.js
@@ -1,8 +1,31 @@
 function flip(array, n) {
+    let j = n - 1;
+    for (let i = 0; i < j; i++) {
+        [array[i], array[j]] = [array[j], array[i]];
+        j--;
+    }
     return array;
 }
 
 // Use only flip() here to manipulate the array
 function pancakeSort(array) {
+    let length = array.length;
+
+    for (let i = length; i > 1; i--) {
+        let largestIndex = 0;
+        for (let j = 1; j < i; j++) {
+            if (array[j] > array[largestIndex]) {
+                largestIndex = j;
+            }
+        }
+
+        if (largestIndex != i - 1){
+            if (largestIndex != 0) {
+                flip(array, largestIndex + 1);
+            }
+            flip (array, i)
+        }
+    }
+        
     return array;
 }
